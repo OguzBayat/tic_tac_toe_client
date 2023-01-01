be0e45a96172485697ead8f056d181cf2fab9260
OguzBayat <oguzbayat77@gmail.com>
2023-01-02 02:05:18 +0300

    Update play fileds when player clicks

--- app/controllers/games/show.js
+++ app/controllers/games/show.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import Ember from 'ember';
-import { alias } from '@ember/object/computed';
 import { service } from '@ember/service';
 
 export default class GamesShowController extends Controller {
@@ -12,24 +11,37 @@ export default class GamesShowController extends Controller {
     let box_id = model.target.id;
     let sign = this.model.turn;
     let move_count = this.model.move_count;
-    let players = this.model.plays;
+    let player1_moves = this.model.player1_moves;
+    let player2_moves = this.model.player2_moves;
+    let move = box_id.slice(-1);
+    if (signs[move_count % 2] === 'X') {
+      player2_moves.push(move);
+    } else {
+      player1_moves.push(move);
+    }
 
     Ember.set(document.getElementById(box_id), 'innerHTML', sign);
+    Ember.set(
+      document.getElementById('turn'),
+      'innerHTML',
+      signs[move_count % 2] + ' Turn Now'
+    );
 
     this.store.findRecord('game', this.model.id).then(function (tyrion) {
       tyrion.set('turn', signs[move_count % 2]);
       tyrion.set('move_count', move_count + 1);
+      tyrion.set('player1_moves', player1_moves.sort());
+      tyrion.set('player2_moves', player2_moves.sort());
       tyrion.save();
     });
   }
 
   addGame() {
-    this.store
-      .createRecord('game', {
-        winner_id: null,
-        move_count: 0,
-        turn: 'X',
-      })
-      .save();
+    let game = this.store.createRecord('game', {
+      winner_id: null,
+      move_count: 0,
+      turn: 'X',
+    });
+    game.save();
   }
 }
--- app/models/game.js
+++ app/models/game.js
@@ -3,5 +3,8 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class GameModel extends Model {
   @attr('number') move_count;
   @attr('string') turn;
+  @attr('number') winner_id;
+  @attr() player1_moves;
+  @attr() player2_moves;
   @hasMany('play') plays;
 }
